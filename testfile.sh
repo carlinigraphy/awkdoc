8157f62c86a4f69c382304b2e0a5e2804668d312
Marcus Aurelius <github-hre-utils@a.aurelius.org>
2022-11-10 17:15:54 -0800

    Initial commit

---
+++ testfile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# @type
+
+
+# @arg   something (jeff)  description1 beep boop this is the description
+# @arg   else      (tim)   description2
+#
+# @does  Does a foo with a bar
+#           - and here's another line of does, because it meets or exceeds the number
+#        of spaces while being directly below it.
+#
+#        This should not be included.
+function foo(){
+   asdfdsa
+}
