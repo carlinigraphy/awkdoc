201f035510870c9ce7c18e3308fec927caf535dd
Marcus Aurelius <github-hre-utils@a.aurelius.org>
2022-11-28 18:07:57 -0800

    Collects `@arg` and `@description` prints as table

---
+++ testfile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# @description      (21), and
+#                   a second line of output.
+#
+# @arg  this     :that     Des|c 1
+# @arg  here     :there
+function foo { :; }
+
+
+# @description
+#  (4), and here is a description on the subsequent line, and...
+#  a second line of output.
+#
+# @arg  here     :there    Desc 1
+# @arg  this     Desc 2
+function foo {
+   return
+}
