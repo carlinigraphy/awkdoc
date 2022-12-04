8de17195bd4accf96fba9218a8930afae3d097f2
Marcus Aurelius <github-hre-utils@a.aurelius.org>
2022-12-03 19:56:57 -0800

    Add @see, fix links to be monospace

--- testfile.sh
+++ testfile.sh
@@ -3,5 +3,9 @@
 # @description      (21), and
 #                   a second line of output.
 #
-# @arg $1 this that
+# @sets THIS THAT
+# @env  HERE THERE
+#
+# @see something:else
+#
 function foo { :; }
