1f66145ca6b1f7e0ae811eb985cba4947f7b0eb4
Marcus Aurelius <github-hre-utils@a.aurelius.org>
2022-12-03 22:03:04 -0800

    Actually fix links to be monospace

--- testfile.sh
+++ testfile.sh
@@ -6,6 +6,23 @@
 # @sets THIS THAT
 # @env  HERE THERE
 #
-# @see something:else
+# @see bar:foo
 #
 function foo { :; }
+
+# @section Beep boop something
+# @description
+
+# @description
+#  (21), and
+#  a second line of output.
+#
+# @sets THIS THAT
+# @env  HERE THERE
+#
+# @see foo
+#
+function bar:foo { :; }
+
+
+
