818421d092b40ff085cd20047f84e7f824e1617f
Marcus Aurelius <github-hre-utils@a.aurelius.org>
2022-12-03 19:22:08 -0800

    Add support for @env and @sets

--- testfile.sh
+++ testfile.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 
-# @section       Something
-# @description   Here's a description
-#                and more stuff here
-#--
-
 # @description      (21), and
 #                   a second line of output.
 #
-# @arg  this     :that     Des|c 1
-# @arg  here     :there
+# @arg $1 this that
 function foo { :; }
-
-
-# @description
-#  (4), and here is a description on the subsequent line, and...
-#  a second line of output.
-#
-#  BIP BIP
-#---
-#
-#  THIS SHOULD NOT BE HERE
-#
-# @arg  here     :there    Desc 1
-# @arg  this               Desc 2    Booooop
-bar ( )
-{
-   return
-}
